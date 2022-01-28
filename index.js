2c10b14a2aa23dc15a1390169f9ca99c69aec838
Daeyoung <daeyoungchoi@hotmail.com>
2022-01-28 00:20:16 -0800

    commit tree 4ea5eb2f88611e57f0a9310d330236cd7a01b20b

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return [drivers[0],drivers[1]]
+}
+
+const drivers = ["charlie","babo","dada","alligator"]
+
+console.log(returnFirstTwoDrivers(drivers))
+
+const returnLastTwoDrivers = function(drivers) {
+    return [drivers[drivers.length-2],drivers[drivers.length-1]]
+}
+
+console.log(returnLastTwoDrivers(drivers))
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+console.log(selectingDrivers)
+
+const createFareMultiplier = function(multiplier) {
+    return function(baseFare) {
+        return baseFare * multiplier
+    }
+}
+
+console.log(createFareMultiplier(4)(40))
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+console.log(fareDoubler(10))
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+console.log(fareTripler(10))
+
+const selectDifferentDrivers = function(drivers,cb) {
+    return cb(drivers);
+}
+
+console.log(selectDifferentDrivers(drivers,returnFirstTwoDrivers))
+
+console.log(selectDifferentDrivers(drivers,returnLastTwoDrivers))
